2adbc0b66a8250fa653c99fb0b9297df6c0c3ac8
Utkarsh Vyas <utkarshvyas200370@gmail.com>
2022-11-28 18:28:23 +0530

    week one

--- code1.py
+++ code1.py
@@ -1,15 +1,20 @@
-print("hello world world")
-print("I'm Utkarsh")
+# print("hello world world")
+# print("I'm Utkarsh")
+#
+# print("hello \"world\" world")
+# print("I \"'m\" utkarsh")
+#
+# print("line1\nline2\nline3")
+# print("name\tUtkarsh")
+# print("this is double backslash\\\\")
+# print("hell\blo")
+#
+# print("line A \\n line B")
+# print("line B \\t line B")
+# print("this is 4 backslash \\\\\\\\")
+# print(" \\\" \\\' ")
 
-print("hello \"world\" world")
-print("I \"'m\" utkarsh")
-
-print("line1\nline2\nline3")
-print("name\tUtkarsh")
-print("this is double backslash\\\\")
-print("hell\blo")
-
-print("line A \\n line B")
-print("line B \\t line B")
-print("this is 4 backslash \\\\\\\\")
-print(" \\\" \\\' ")
+print("this is \\\\ double backslash")
+print("these are /\\/\\/\\/\\ mountains")
+print("he is\tawesome")
+print(" \\\" \\n \\t \\\' ")
